80adae82e356d7437ac22a61c0b72bbc2c45e0bd
Sahil Talreja <Talrejasahil4994@gmail.com>
2026-02-09 15:23:26 +0530

    added redis config file

---
+++ src/config/redis.js
@@ -0,0 +1,20 @@
+
+import { createClient } from redis;
+
+const redis = createClient({
+    url: process.env.REDIS_URL || "redis://127.0.0.1:6379"
+});
+
+redis.on("connect", () => {
+    console.log("🧠 Redis connected");
+});
+
+redis.on("error", (err) => {
+    console.error("❌ Redis error", err);
+});
+
+(async () => {
+    await redis.connect();
+})();
+
+export default redis;
